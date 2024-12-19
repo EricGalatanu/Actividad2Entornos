64861fd899c4da3523cd0527c27099c3c719856f
EricGalatanu <egalatanu.programador@gmail.com>
2024-12-19 15:56:13 +0100

    Titulo
    Titulo de la actividad

---
+++ src/ActividadEntornos/Actividad.java
@@ -0,0 +1,14 @@
+package ActividadEntornos;
+
+public class Actividad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Actividad: Gestión de repositorios en GitHub y desarrollo de una aplicación en Java");
+		System.out.println("By Santi Corrales, Eric Galatanu, Quim Pueyo");
+		System.out.println();
+		
+	}
+
+}
